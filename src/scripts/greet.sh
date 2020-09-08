00cba0536a2457d47e1c26f42daef75b37383ca2
Kyle T <33272306+KyleTryon@users.noreply.github.com>
2020-09-08 13:25:30 -0400

    more specific variable

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -4,7 +4,7 @@ Greet() {
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
-TEST_ENV="bats-core"
-if [ "${0#*$TEST_ENV}" == "$0" ]; then
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
     Greet
 fi
